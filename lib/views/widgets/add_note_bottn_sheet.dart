2e8cd3a3ab4c66b28d998d5e6438993ef9066931
Muradfs <113851806+Muradfs@users.noreply.github.com>
2024-11-17 20:35:28 +0300

    finished note button Sheet

--- lib/views/widgets/add_note_bottn_sheet.dart
+++ lib/views/widgets/add_note_bottn_sheet.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/constants.dart';
 import 'package:note_app/views/widgets/custom_text_field.dart';
+
+import 'custom_button.dart';
 
 class AddNoteBottonSheet extends StatelessWidget {
   const AddNoteBottonSheet({super.key});
@@ -8,22 +11,32 @@ class AddNoteBottonSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextField(
-            hint: 'title',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            hint: 'Content',
-            maxlines: 5,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextField(
+              hint: 'title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hint: 'Content',
+              maxlines: 5,
+            ),
+            // Spacer(), it is not allowed inside SingleChildScrollView
+            SizedBox(
+              height: 32,
+            ),
+            CustonBotton(),
+            SizedBox(
+              height: 8,
+            ),
+          ],
+        ),
       ),
     );
   }
