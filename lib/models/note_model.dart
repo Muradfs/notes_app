6af25a540348223018ed018f11208d37f234843a
Muradfs <113851806+Muradfs@users.noreply.github.com>
2024-11-18 20:37:15 +0300

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final String date;
+  final int color;
+
+  NoteModel({
+    required this.title,
+    required this.subTitle,
+    required this.Date,
+    required this.color,
+  });
+}
